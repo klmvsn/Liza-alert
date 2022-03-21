55c64038edc288f1c2f7cf0df2ac62b0f4bafb01
Evgeny Rusakov <shoomec@yandex.ru>
2022-03-21 22:51:56 +0500

    Все переходы по кнопкам работают в соответствии с брифом

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,42 +1,50 @@
 const allButtons = document.querySelectorAll('.button');
 
-switch(location.pathname) {
-    case '/video.html':
-    transition(allButtons, '#', './test-preview.html', '#');
-    case '/test.html':
-    transition(allButtons, '#', './test-preview.html', '#');
-    case '/test-preview.html':
-    transition(allButtons, '#', './test-preview.html', './test.html');
-    case '/test-good.html':
-    transition(allButtons, '#', './test-preview.html', './test.html');
-    case '/test-bad.html':
-    transition(allButtons, '#', './test-preview.html', './test.html');
-    case '/test-about.html':
-    transition(allButtons, '#', './test-preview.html', '#');
-    case '/course-end_passed.html':
-    transition(allButtons, '#', './test-preview.html', '#');
-    case '/course-end_not-passed.html':
-    transition(allButtons, '#', './test-preview.html', '#');
-    default:
-        transition(allButtons, '#', '#', '#');
+function transition(buttons, linkForButtonBack, linkForButtonNext, linkForButtonResult) {
+  buttons.forEach(button => {
+    if (button.name === 'back') {
+      button.addEventListener('click', () => {
+        location.href = linkForButtonBack;
+      });
+    }
+    else if (button.name === 'next') {
+      button.addEventListener('click', () => {
+        location.href = linkForButtonNext;
+      });
+    }
+    else if (button.name === 'result') {
+      button.addEventListener('click', () => {
+        location.href = linkForButtonResult;
+      });
+    }
+  });
 }
-
-function transition (buttons, backLink, nextLink, resultLink) {
-    buttons.forEach(element => {
-        if (element.name === 'next') {
-            element.addEventListener('click', () =>{
-                location.href = nextLink;
-            });
-        }
-        else if (element.name === 'back') {
-            element.addEventListener('click', () =>{
-                location.href = backLink;
-            });
-        }
-        else if (element.name === 'result') {
-            element.addEventListener('click', () =>{
-                location.href = resultLink;
-            });
-        }
-    });
+// ('кнопки', 'ссылка для кнопки назад', 'ссылка для кнопки далее', 'ссылка для кнопки card-block') //
+switch (location.pathname) {
+  case '/video.html':
+    transition(allButtons, '#', './test-preview.html', '#');
+    break;
+  case '/test-preview.html':
+    transition(allButtons, './video.html', '#', './test.html');
+    break;
+  case '/test.html':
+    transition(allButtons, './test-preview.html', '#', '#');
+    break;
+  case '/test-about.html':
+    transition(allButtons, './test.html', '#', '#');
+    break;
+  case '/test-bad.html':
+    transition(allButtons, './video.html', './course-end_not-passed.html', './test.html');
+    break;
+  case '/test-good.html':
+    transition(allButtons, './video.html', './course-end_passed.html', './test.html');
+    break;
+  case '/course-end_passed.html':
+    transition(allButtons, './test-good.html', '#', '#');
+    break;
+  case '/course-end_not-passed.html':
+    transition(allButtons, './test-bad.html', '#', '#');
+    break;
+  default:
+    transition(allButtons, '#', '#', '#');
 }
